7deb19a125aa2193f89d7eab239ba62c36e474be
yonghyun <dhappen@naver.com>
2021-01-25 16:37:09 +0900

    2021.01.25(mon) affine complete

--- DIP_week1/main.py
+++ DIP_week1/main.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import numpy as np
 import math
+import sys, time
 # from intensityTF import intensityTF
 
 class forwardaffineTF:
@@ -84,30 +85,7 @@ class forwardaffineTF:
         cv2.imshow("affine",imgtf)
         cv2.imshow("affine_original",self.img)
         cv2.imwrite(self.output+'[{0}]forwardAffineTF.tif'.format(self.imgname),imgtf)
-# class interpolation:
-#     def __init__(self,int_type,img):
-#         self.int_type = int_type
-#         self.img = img
-#     def excute(self,pixel):
-#         p = 0
-#         if self.int_type == "nearest":
-#             if pixel >= 0:
-#                 p = self.img(pixel(1, 0), pixel(0, 0))
-#             else:
-#                 p = 0
-#         elif self.int_type == "bilinear":
-#             if pixel >= 0:
-#                 p = self.img(pixel(1, 0), pixel(0, 0))
-#             else:
-#                 p = 0
-#
-#         elif self.int_type == "bicubic":
-#             if pixel >= 0:
-#                 p = self.img(pixel(1, 0), pixel(0, 0))
-#             else:
-#                 p = 0
-#
-#         return p
+
 
 class inverseAffine:
     def __init__(self,img,output,imgname,int_type):
@@ -125,11 +103,16 @@ class inverseAffine:
                 return i1 * (m+1-kpixel) + i2 * (kpixel - m)
             def cubic(i1,i2,i3,i4,kpixel):
                 m = kpixel - int(kpixel)
+                
                 t = np.array([1,m,m*m,m*m*m],dtype=np.float64)
-                f = np.array([i1,i2,i3,i4]).reshape((4,1))
+                f = np.array([i1,i2,i3,i4],dtype=np.float64).reshape((4,1))
                 tm = np.dot(t,cubicmat)
-                pt = np.dot(tm,f)
-                return pt/2
+                pt = 0.5 * np.dot(tm,f)
+                if pt > 255:
+                    pt = 255
+                elif pt < 0:
+                    pt = 0
+                return pt
             p = 0
 
             kx = pixel[0][0]
@@ -138,12 +121,12 @@ class inverseAffine:
             b = int(ky)
 
             if type == "nearest":
-                if (0<=pixel).all and a < img.shape[1]-1 and b < img.shape[0]-1:
+                if 0 <= a < self.width-1 and 0 <= b < self.height-1:
                     p = img[int(ky+0.5)][int(kx+0.5)]
                 else:
                     p = 0
             elif type == "bilinear":
-                if (0<=pixel).all and a < img.shape[1]-1 and b < img.shape[0]-1:
+                if 0 <= a < self.width-1 and 0 <= b < self.height-1:
                     x1 = linear(img[b][a],img[b][a+1],kx)
                     x2 = linear(img[b+1][a],img[b+1][a+1],kx)
                     y = linear(x1,x2,ky)
@@ -151,62 +134,60 @@ class inverseAffine:
                 else:
                     p = 0
             elif type == "bicubic":
-                if (0<pixel).all and a < img.shape[1]-2 and b < img.shape[0]-2:
+                if 0 <= a < self.width-2 and 0 <= b < self.height-2:
                     b1 = cubic(img[b-1][a-1],img[b-1][a],img[b-1][a+1],img[b-1][a+2],kx)
                     b2 = cubic(img[b][a-1],img[b][a],img[b][a+1],img[b][a+2],kx)
                     b3 = cubic(img[b+1][a-1],img[b+1][a],img[b+1][a+1],img[b+1][a+2],kx)
                     b4 = cubic(img[b+2][a-1],img[b+2][a],img[b+2][a+1],img[b+2][a+2],kx)
-                    p = int(cubic(b1,b2,b3,b4,ky))
+                    p = cubic(b1,b2,b3,b4,ky)
+                    
 
                 else:
                     p = 0
             return p
-
+        start_time = time.time()
         a = self.width / 2
         b = self.height / 2
         m = np.eye(3)
         v = np.array([1, 1, 1]).reshape(3, 1)
         w = np.array([1, 1, 1]).reshape(3, 1)
         if type == "zoom" or type == "shirink":
-            m[0][0] = 1/k
-            m[1][1] = 1/j
+            m[0][0] = k
+            m[1][1] = j
 
         elif type == "rotation":
             m[0][0] = np.cos(theta * math.pi / 180)
-            m[0][1] = np.sin(theta * math.pi / 180)
-            m[1][0] = -np.sin(theta * math.pi / 180)
+            m[0][1] = -np.sin(theta * math.pi / 180)
+            m[1][0] = np.sin(theta * math.pi / 180)
             m[1][1] = np.cos(theta * math.pi / 180)
-            m[0][2] = a - a * np.cos(theta * math.pi / 180) \
-                      - b * np.sin(theta * math.pi / 180)
-            m[1][2] = b - b * np.cos(theta * math.pi / 180) \
-                      + a * np.sin(theta * math.pi / 180)
+            m[0][2] = a - a * np.cos(theta * math.pi / 180) + b * np.sin(theta * math.pi / 180)
+            m[1][2] = b - b * np.cos(theta * math.pi / 180) - a * np.sin(theta * math.pi / 180)
 
         elif type == "trans":
-            m[0][2] = -k
-            m[1][2] = -j
+            m[0][2] = k
+            m[1][2] = j
         elif type == "shear":
-            m[0][0] = -1/(k*j-1)
-            m[0][1] = j/(k*j-1)
-            m[1][0] = k/(k*j-1)
-            m[1][1] = -1/(k*j-1)
+            # m[0][0] = -1/(k*j-1)
+            m[0][1] = j
+            m[1][0] = k
+            # m[1][1] = -1/(k*j-1)
         else:
             print("type이 잘 못 되었습니다.")
         '''출력 이미지 size 측정 및 출력 후 이미지 위치 조정'''
         m_inv = np.linalg.inv(m)
         v[0][0] = 0
         v[1][0] = 0
-        w1 = np.dot(m_inv,v)
+        w1 = np.dot(m,v)
         v[0][0] = 2 * a
         v[1][0] = 0
-        w2 = np.dot(m_inv,v)
+        w2 = np.dot(m,v)
         v[0][0] = 0
         v[1][0] = 2 * b
-        w3 = np.dot(m_inv,v)
+        w3 = np.dot(m,v)
         v[0][0] = 2 * a
         v[1][0] = 2 * b
-        w4 = np.dot(m_inv,v)
+        w4 = np.dot(m,v)
         #################################################
-
         l0 = np.zeros((3, 1))
         l = np.hstack([w1, w2, w3, w4,l0])
         lx = int(np.max(l[0][0:]) - np.min(l[0][0:]))
@@ -220,29 +201,45 @@ class inverseAffine:
             adj_y = np.min(l[1][0:])
         else:
             adj_y = 0
+        print('It will take a little while...')
+        inc = 0
         for yy in range(ly):
             for xx in range(lx):
                 v[0][0] = int(xx + adj_x)
                 v[1][0] = int(yy + adj_y)
-                w = np.dot(m, v)
-                if yy == 1 and xx == 1:
-                    print(w)
+                # v[0][0] = xx
+                # v[1][0] = yy
+                w = np.dot(m_inv, v)
                 imgtf[yy][xx] = interpolation(self.int_type,self.img,w)
+                inc = inc + 1
+                sys.stderr.write('\r\033[K' + get_progressbar_str(inc/(imgtf.size)))
+                sys.stderr.flush() 
+        sys.stderr.write('\n')
+        sys.stderr.flush()
 
-
+        
         cv2.imshow("affine[{0}]".format(self.int_type), imgtf)
-
-        cv2.imwrite(self.output + '[{0}]inverseAffineTF.tif'.format(self.imgname), imgtf)
+        cv2.imwrite(self.output + '[{0}]inverseAffineTF[{1}]_[{2}].tif'.format(self.imgname,type,self.int_type), imgtf)
         cv2.imshow("affine_original", self.img)
+        print("affine_[{0}]_[{1}]_소요시간 : [{2}]sec".format(type,self.int_type,time.time()-start_time))
+        print("-----------------finish the transform-----------------")
+
+
+def get_progressbar_str(progress):
+    END = 170
+    MAX_LEN = 30
+    BAR_LEN = int(MAX_LEN * progress)
+    return ('Progress:[' + '=' * BAR_LEN +
+            ('>' if BAR_LEN < MAX_LEN else '') +
+            ' ' * (MAX_LEN - BAR_LEN) +
+            '] %.1f%%' % (progress * 100.))
 
 
 
 
 
 
-
-
-
+os.chdir("DIP_week1")    ##학교 컴퓨터일때
 folder="picture/"
 output="output/"
 breast = "Fig0304(a)(breast_digital_Xray).tif"
@@ -254,6 +251,7 @@ contrast = "Fig0310(b)(washed_out_pollen_image).tif"
 kidney = "Fig0312(a)(kidney).tif"
 dollor = "Fig0314(a)(100-dollars).tif"
 T = "Fig0236(a)(letter_T).tif"
+watch = "Fig0220(a)(chronometer 3692x2812  2pt25 inch 1250 dpi).tif"
 # breast_img = cv2.imread(folder+breast,cv2.IMREAD_GRAYSCALE)
 # fourier_img = cv2.imread(folder+fourier,cv2.IMREAD_GRAYSCALE)
 # ramp_img = cv2.imread(folder+ramp,cv2.IMREAD_GRAYSCALE)
@@ -263,7 +261,7 @@ contrast_img = cv2.imread(folder+contrast,cv2.IMREAD_GRAYSCALE)
 kidney_img = cv2.imread(folder+kidney,cv2.IMREAD_GRAYSCALE)
 dollor_img = cv2.imread(folder+dollor,cv2.IMREAD_GRAYSCALE)
 T_img = cv2.imread(folder+T,cv2.IMREAD_GRAYSCALE)
-
+watch_img = cv2.imread(folder+watch,cv2.IMREAD_GRAYSCALE)
 # intensityTF(breast_img,output,"breast").negative()
 # intensityTF(fourier_img,output,"fourier").logtransform()
 # intensityTF(ramp_img,output,"ramp").gammatransform(2.5)
@@ -278,8 +276,13 @@ T_img = cv2.imread(folder+T,cv2.IMREAD_GRAYSCALE)
 # intensityTF(kidney_img,output,"slicing").slicing()
 # intensityTF(dollor_img,output,"dollor").bitplane()
 # forwardaffineTF(T_img, output, "T").TF("rotation",2,30)
-inverseAffine(T_img,output,"T","bicubic").affine("rotation",2,1.5,30)
-# inverseAffine(T_img,output,"T","nearest").affine("zoom",2,1.5,30)
+# inverseAffine(Aerial_img,output,"Aerial","nearest").affine("rotation",2,2,30)
+# inverseAffine(T_img,output,"T","bicubic").affine("rotation",2,1.5,30)
+# inverseAffine(T_img,output,"T","bilinear").affine("rotation",2,1.5,30)
+# inverseAffine(T_img,output,"T","nearest").affine("rotation",2,1.5,30)
+inverseAffine(watch_img,output,"watch","bicubic").affine("zoom",1.2,1.2,30)
+inverseAffine(watch_img,output,"watch","bilinear").affine("zoom",1.2,1.2,30)
+inverseAffine(watch_img,output,"watch","nearest").affine("zoom",1.2,1.2,30)
 '''nearest or bilinear or bicubic '''
 # print(type(img[0][0]))
 # print(type(neg_img[0][0]))
--- DIP_week1/test.py
+++ DIP_week1/test.py
@@ -1,27 +1,103 @@
+import cv2
 import numpy as np
+import math
+import sys, time
+import os
 
+# Interpolation kernel
+def u(s,a):
+    if (abs(s) >=0) & (abs(s) <=1):
+        return (a+2)*(abs(s)**3)-(a+3)*(abs(s)**2)+1
+    elif (abs(s) > 1) & (abs(s) <= 2):
+        return a*(abs(s)**3)-(5*a)*(abs(s)**2)+(8*a)*abs(s)-4*a
+    return 0
 
-# x = np.eye(4)
-# x = x+1
-# print(x)
-# x[0][1] = np.cos(60)
-# print(x)
+#Paddnig
+def padding(img,H,W,C):
+    zimg = np.zeros((H+4,W+4,C))
+    zimg[2:H+2,2:W+2,:C] = img
+    #Pad the first/last two col and row
+    zimg[2:H+2,0:2,:C]=img[:,0:1,:C]
+    zimg[H+2:H+4,2:W+2,:]=img[H-1:H,:,:]
+    zimg[2:H+2,W+2:W+4,:]=img[:,W-1:W,:]
+    zimg[0:2,2:W+2,:C]=img[0:1,:,:C]
+    #Pad the missing eight points
+    zimg[0:2,0:2,:C]=img[0,0,:C]
+    zimg[H+2:H+4,0:2,:C]=img[H-1,0,:C]
+    zimg[H+2:H+4,W+2:W+4,:C]=img[H-1,W-1,:C]
+    zimg[0:2,W+2:W+4,:C]=img[0,W-1,:C]
+    return zimg
 
-# v = np.array([1,1,1]).reshape(3,1)
-# print(v)
-# m = np.eye(3)
-# v = np.array([1,1,1]).reshape(3,1)
-# w = np.array([1,1,1]).reshape(3,1)
-# m[0,0] = 6
-# m[1,0] = 3
-# w = np.dot(m,v)
-# l = np.hstack([v,w,v,w,v,w])
-# lx = np.max(l[0,:]) - np.min(l[0,:])
-# print(lx)
-# print(l)
-# print(m)
-# print(v)
-# print(w)
-# print(abs(v-w))
-m = np.cos(180)
-print(m)
+# https://github.com/yunabe/codelab/blob/master/misc/terminal_progressbar/progress.py
+def get_progressbar_str(progress):
+    END = 170
+    MAX_LEN = 30
+    BAR_LEN = int(MAX_LEN * progress)
+    return ('Progress:[' + '=' * BAR_LEN +
+            ('>' if BAR_LEN < MAX_LEN else '') +
+            ' ' * (MAX_LEN - BAR_LEN) +
+            '] %.1f%%' % (progress * 100.))
+
+# Bicubic operation
+def bicubic(img, ratio, a):
+    #Get image size
+    H,W,C = img.shape
+
+    img = padding(img,H,W,C)
+    #Create new image
+    dH = math.floor(H*ratio)
+    dW = math.floor(W*ratio)
+    dst = np.zeros((dH, dW, 3))
+
+    h = 1/ratio
+
+    print('Start bicubic interpolation')
+    print('It will take a little while...')
+    inc = 0
+    for c in range(C):
+        for j in range(dH):
+            for i in range(dW):
+                x, y = i * h + 2 , j * h + 2
+
+                x1 = 1 + x - math.floor(x)
+                x2 = x - math.floor(x)
+                x3 = math.floor(x) + 1 - x
+                x4 = math.floor(x) + 2 - x
+
+                y1 = 1 + y - math.floor(y)
+                y2 = y - math.floor(y)
+                y3 = math.floor(y) + 1 - y
+                y4 = math.floor(y) + 2 - y
+
+                mat_l = np.matrix([[u(x1,a),u(x2,a),u(x3,a),u(x4,a)]])
+                mat_m = np.matrix([[img[int(y-y1),int(x-x1),c],img[int(y-y2),int(x-x1),c],img[int(y+y3),int(x-x1),c],img[int(y+y4),int(x-x1),c]],
+                                   [img[int(y-y1),int(x-x2),c],img[int(y-y2),int(x-x2),c],img[int(y+y3),int(x-x2),c],img[int(y+y4),int(x-x2),c]],
+                                   [img[int(y-y1),int(x+x3),c],img[int(y-y2),int(x+x3),c],img[int(y+y3),int(x+x3),c],img[int(y+y4),int(x+x3),c]],
+                                   [img[int(y-y1),int(x+x4),c],img[int(y-y2),int(x+x4),c],img[int(y+y3),int(x+x4),c],img[int(y+y4),int(x+x4),c]]])
+                mat_r = np.matrix([[u(y1,a)],[u(y2,a)],[u(y3,a)],[u(y4,a)]])
+                dst[j, i, c] = np.dot(np.dot(mat_l, mat_m),mat_r)
+
+                # Print progress
+                inc = inc + 1
+                sys.stderr.write('\r\033[K' + get_progressbar_str(inc/(C*dH*dW)))
+                sys.stderr.flush()
+    sys.stderr.write('\n')
+    sys.stderr.flush()
+    return dst
+os.chdir("DIP_week1")    ##학교 컴퓨터일때
+folder="picture/"
+output="output/"
+T = "Fig0236(a)(letter_T).tif"
+# Read image
+img = cv2.imread(folder+T)
+zoom = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC) 
+# Scale factor
+ratio = 2
+# Coefficient
+a = -1/2
+
+# dst = bicubic(img, ratio, a)
+print('Completed!')
+cv2.imshow("x",zoom)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
